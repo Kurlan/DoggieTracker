7a99f9c6667e168a2a90aeaab57a2d2618748219
Steve Huynh <stevoh@gmail.com>
2015-10-10 16:01:27 -0700

    Changing directory structure

---
+++ DoggieTracker/Models/Dog.swift
@@ -0,0 +1,58 @@
+//
+//  Dog.swift
+//  DoggieTracker
+//
+//  Created by STEVE HUYNH on 10/8/15.
+//  Copyright © 2015 SLH. All rights reserved.
+//
+
+import UIKit
+
+
+class Dog: NSObject, NSCoding {
+    // MARK: Properties
+    
+    static let DocumentsDirectory = NSFileManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first!
+    
+    static let ArchiveURL = DocumentsDirectory.URLByAppendingPathComponent("dogs")
+    
+    struct PropertyKey {
+        static let nameKey = "name"
+        static let photoKey = "photo"
+        static let ratingKey = "rating"
+    }
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    // MARK: Initialization
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+        super.init()
+        
+        if name.isEmpty || rating < 0 {
+            return nil
+        }
+        
+    }
+    
+    // MARK: NSCoding
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(name, forKey: PropertyKey.nameKey)
+        aCoder.encodeObject(photo, forKey: PropertyKey.photoKey)
+        aCoder.encodeInteger(rating, forKey: PropertyKey.ratingKey)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        let name = aDecoder.decodeObjectForKey(PropertyKey.nameKey) as! String
+        let photo = aDecoder.decodeObjectForKey(PropertyKey.photoKey) as? UIImage
+        let rating = aDecoder.decodeIntegerForKey(PropertyKey.ratingKey)
+        self.init(name:name, photo:photo, rating: rating)
+    }
+}
